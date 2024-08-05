cd9caebdc349ed987e771f0deeaad8bd1c36e5f8
KillianFvt <killian.fauvette@gmail.com>
2024-08-05 19:02:01 +0200

    updated worker headers to send CORS policy

--- upload-r2-assets/src/index.ts
+++ upload-r2-assets/src/index.ts
@@ -3,35 +3,52 @@ interface Env {
 	AUTH_SECRET: string;
 }
 
+const corsHeaders = {
+	'Access-Control-Allow-Headers': '*',
+	'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
+	'Access-Control-Allow-Origin': '*',
+}
+
 export default {
 	async fetch(request, env: Env): Promise<Response> {
+
+		if (request.method === 'OPTIONS') {
+			return new Response(null, {
+				status: 200,
+				headers: corsHeaders,
+			});
+		}
 
 		const auth: string | null = request.headers.get('Authorization');
 		const expectedAuth: string = `Bearer ${env.AUTH_SECRET}`;
 
 		if (!auth || auth !== expectedAuth) {
-			return new Response('Unauthorized', { status: 401 });
+			return new Response('Unauthorized', {
+				status: 401,
+				headers: corsHeaders,
+			});
 		}
 
 		if (request.method === 'PUT' || request.method === 'POST') {
-			// For example, the request URL my-worker.account.workers.dev/image.png
 			const url: URL = new URL(request.url);
-			// the key is "image.png"
 			const key: string = url.pathname.slice(1);
 			await env.KF_R2_BUCKET.put(key, request.body);
-			return new Response(`Object ${key} uploaded successfully!`);
+			return new Response(
+				`Object ${key} uploaded successfully!`,
+				{
+					status: 201,
+					headers: corsHeaders,
+				},
+			);
 		}
 
 		if (request.method === 'GET') {
-			// For example, the request URL my-worker.account.workers.dev/image.png
 			const url: URL = new URL(request.url);
-			// the key is "image.png"
 			const key: string = url.pathname.slice(1);
-			// Retrieve the key "image.png"
 			const object: R2ObjectBody | null = await env.KF_R2_BUCKET.get(key);
 
 			if (object === null) {
-				return new Response(`Can\'t find ${key}`, { status: 404 });
+				return new Response(`Can't find ${key}`, { status: 404, headers: corsHeaders });
 			}
 
 			const headers = new Headers();
@@ -39,7 +56,7 @@ export default {
 			headers.set('etag', object.httpEtag);
 
 			return new Response(object.body, {
-				headers,
+				headers: { ...headers, ...corsHeaders },
 			});
 		}
 
@@ -47,9 +64,15 @@ export default {
 			const url = new URL(request.url);
 			const key = url.pathname.slice(1);
 			await env.KF_R2_BUCKET.delete(key);
-			return new Response(`Object ${key} deleted successfully!`);
+			return new Response(
+				`Object ${key} deleted successfully!`,
+				{
+					status: 204,
+					headers: corsHeaders,
+				},
+			);
 		}
 
-		return new Response('Method Not Allowed', { status: 405 });
+		return new Response('Method Not Allowed', { status: 405, headers: corsHeaders });
 	},
 } satisfies ExportedHandler<Env>;
